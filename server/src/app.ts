90f3784411c8ec1ff26b2f1e189d466d652cc750
Kris Casper <sw.darkwind@gmail.com>
2021-07-06 22:10:29 +0300

    update server routing for SPA

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const publicPath = path.resolve(__dirname, '../wwwroot');
+const indexPath = path.resolve(__dirname, '../wwwroot/index.html');
+
+// if query not starts with '/api/' string - send file from wwwroot
 app.use(/^(?!\/api\/)/, express.static(publicPath));
+// if file doesn't exists - send index.html
+app.use(/^(?!\/api\/)/, (req, res) => {
+  res.sendFile(indexPath);
+});
 app.use('/api/categories', categories);
 app.use('/api/items', items);
 
